c64ea8d243de3ecc1946192a2b933465f46bbfa2
konarkm <mail@konarkm.com>
2024-07-04 20:33:41 -0700

    Refactor code to use main function idiom for better modularity

--- app.py
+++ app.py
@@ -5,61 +5,79 @@ import streamlit as st
 import openai
 import time
 
-st.title("OpenAI Assistants API UI")
 
-# Load environment variables if user input is disabled
-load_dotenv()
 
-user_input = os.environ.get("USER_INPUT", "True")
+def get_client():
+    # Load environment variables if user input is disabled
+    load_dotenv()
 
-# Check if user input is disabled
-if user_input == "False":
-    api_key = os.environ.get("OPENAI_API_KEY")
-else:
-    # Ask for user's OpenAI Key
-    api_key = st.text_input("Enter your OpenAI Key", type="password")
+    user_input = os.environ.get("USER_INPUT", "True")
 
-# Authenticate with OpenAI
-client = openai.OpenAI(api_key=api_key)
+    # Check if user input is disabled
+    if user_input == "False":
+        api_key = os.environ.get("OPENAI_API_KEY")
+    else:
+        # Ask for user's OpenAI Key
+        api_key = st.text_input("Enter your OpenAI Key", type="password")
 
-# List all assistants from your account
-all_assistants = client.beta.assistants.list(
-    order="desc",
-    limit="20",
-)
-# Create a dictionary of assistant names and IDs
-assistants_dict = {}
-for assistant in all_assistants.data:
-    assistants_dict[assistant.name] = assistant.id
+    # Authenticate with OpenAI
+    client = openai.OpenAI(api_key=api_key)
 
-# Select an assistant
-selected_assistant = st.selectbox("Select Assistant", list(assistants_dict.keys()), index=0)
-
-# Get the selected assistant ID
-selected_assistant_id = assistants_dict.get(selected_assistant)
-
-# Save the selected assistant ID to the session state
-st.session_state["assistant_id"] = selected_assistant_id
-
-# The assistant's vector store ID can be retrieved when creating the assistants_dict, but is being done here for code readability.
-# Retrieve the selected assistant's information
-selected_assistant_info = client.beta.assistants.retrieve(st.session_state["assistant_id"])
-
-# Get the selected assistant's vector store ID
-try:
-    vector_store_id = selected_assistant_info.tool_resources.file_search.vector_store_ids[0]
-except:
-    st.error("Error retrieving the selected assistant's vector store ID. This can happen if the assistant does not have a vector store.")
-    st.stop()
-    
-
-vector_store = client.beta.vector_stores.retrieve(
-  vector_store_id=vector_store_id
-)
+    return client
 
 
 
-def get_files_dict():
+def select_assistant(client):
+    # List all assistants from your account
+    all_assistants = client.beta.assistants.list(
+        order="desc",
+        limit="20",
+    )
+    # Create a dictionary of assistant names and IDs
+    assistants_dict = {}
+    for assistant in all_assistants.data:
+        assistants_dict[assistant.name] = assistant.id
+
+    # Select an assistant
+    selected_assistant = st.selectbox("Select Assistant", list(assistants_dict.keys()), index=0)
+
+    # Get the selected assistant ID
+    selected_assistant_id = assistants_dict.get(selected_assistant)
+
+    # Save the selected assistant ID to the session state
+    st.session_state["assistant_id"] = selected_assistant_id
+
+    return
+
+
+
+def get_vector_store_id(client):
+
+    # The assistant's vector store ID can be retrieved when creating the assistants_dict, but is being done here for code readability.
+
+    # Check if an vector store ID exists in the session state, otherwise get it from the API
+    if "vector_store_id" not in st.session_state:
+        # Retrieve the selected assistant's information
+        selected_assistant_info = client.beta.assistants.retrieve(st.session_state["assistant_id"])
+
+        # Get the selected assistant's vector store ID
+        try:
+            vector_store_id = selected_assistant_info.tool_resources.file_search.vector_store_ids[0]
+        except:
+            st.error("Error retrieving the selected assistant's vector store ID. This can happen if the assistant does not have a vector store.")
+            st.stop()      
+
+        # vector_store = client.beta.vector_stores.retrieve(
+        # vector_store_id=vector_store_id
+        # )
+    else:
+        vector_store_id = st.session_state["vector_store_id"]
+
+    return vector_store_id
+
+
+
+def get_files_dict(client, vector_store_id):
     # Get the list of files in the vector store
     vector_store_files = client.beta.vector_stores.files.list(
     vector_store_id=vector_store_id
@@ -78,7 +96,7 @@ def get_files_dict():
     return vector_store_files_dict
 
 # Display files
-def display_files():
+def display_files(vector_store_files_dict):
     # Display the list of files in the vector store
     st.sidebar.subheader("Files in Vector Store")
     if not vector_store_files_dict:
@@ -89,9 +107,8 @@ def display_files():
             st.sidebar.markdown(f"- {file}")
 
 
-
 # Upload file menu
-def upload_file():
+def upload_file(client, vector_store_files_dict, vector_store_id):
     if "file_uploader_key" not in st.session_state:
         st.session_state["file_uploader_key"] = 0
 
@@ -128,7 +145,7 @@ def upload_file():
             st.rerun()
 
 
-def delete_file():
+def delete_file(client, vector_store_files_dict, vector_store_id):
     # Select a file to delete
     selected_file = st.sidebar.selectbox("Select File to Delete", list(vector_store_files_dict.keys()))
     file_id_to_delete = vector_store_files_dict.get(selected_file)
@@ -148,75 +165,92 @@ def delete_file():
             st.sidebar.error("No file selected")
 
 
-# Add a streamlit sidebar
-st.sidebar.title("Files")
+def get_thread(client):
+    # Check if a thread ID already exists in the session state, otherwise create a new thread
+    if "thread_id" not in st.session_state:
+        thread = client.beta.threads.create()
+        st.session_state["thread_id"] = thread.id
+    else:
+        thread = client.beta.threads.retrieve(st.session_state["thread_id"])
+    return thread
 
 
+def display_messages():
+    # Check if a messages list already exists in the session state, otherwise create a new list
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    # Display chat messages
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
 
 
-vector_store_files_dict = get_files_dict()
+def chat(client, thread):
+    if prompt := st.chat_input("Enter a messsage"):
+        # Add user message to chat history
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        # Display user message in chat message container
+        with st.chat_message("user"):
+            st.markdown(prompt)
+        
+        # Send user message to assistant
+        message = client.beta.threads.messages.create(
+            thread_id=thread.id,
+            role="user",
+            content=prompt
+        )
 
-
-display_files()
-
-
-upload_file()
-
-
-
-
-delete_file()
-
-# Chat UI
-# Check if a thread ID already exists in the session state, otherwise create a new thread
-if "thread_id" not in st.session_state:
-    thread = client.beta.threads.create()
-    st.session_state["thread_id"] = thread.id
-else:
-    thread = client.beta.threads.retrieve(st.session_state["thread_id"])
-
-# Check if a messages list already exists in the session state, otherwise create a new list
-if "messages" not in st.session_state:
-    st.session_state.messages = []
-
-# Display chat messages
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
-    
-
-if prompt := st.chat_input("Enter a messsage"):
-    # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    # Display user message in chat message container
-    with st.chat_message("user"):
-        st.markdown(prompt)
-    
-    # Send user message to assistant
-    message = client.beta.threads.messages.create(
+        # Run assistant with user input
+        run = client.beta.threads.runs.create_and_poll(
         thread_id=thread.id,
-        role="user",
-        content=prompt
-    )
+        assistant_id=st.session_state["assistant_id"],
+        #    instructions=""
+        )
 
-    # Run assistant with user input
-    run = client.beta.threads.runs.create_and_poll(
-       thread_id=thread.id,
-       assistant_id=st.session_state["assistant_id"],
-    #    instructions=""
-    )
+        # Get messages from the thread
+        messages = client.beta.threads.messages.list(
+            thread_id=thread.id
+        )
 
-    # Get messages from the thread
-    messages = client.beta.threads.messages.list(
-        thread_id=thread.id
-    )
+        # Get the latest assistant response
+        response = messages.data[0].content[0].text.value
 
-    # Get the latest assistant response
-    response = messages.data[0].content[0].text.value
+        # Display assistant response in chat message container
+        with st.chat_message("assistant"):
+            st.markdown(response)
 
-    # Display assistant response in chat message container
-    with st.chat_message("assistant"):
-        st.markdown(response)
+        # Add assistant response to chat history
+        st.session_state.messages.append({"role": "assistant", "content": response})
 
-    # Add assistant response to chat history
-    st.session_state.messages.append({"role": "assistant", "content": response})
+
+
+def main():
+    
+    # Set the title of the streamlit app
+    st.title("OpenAI Assistants API UI")
+    # Add a streamlit sidebar
+    st.sidebar.title("Files")
+
+    client = get_client()
+    select_assistant(client)
+
+    vector_store_id = get_vector_store_id(client)
+
+    vector_store_files_dict = get_files_dict(client, vector_store_id)
+
+    display_files(vector_store_files_dict)
+
+    upload_file(client, vector_store_files_dict, vector_store_id)
+
+    delete_file(client, vector_store_files_dict, vector_store_id)
+
+    thread = get_thread(client)
+
+    display_messages()
+
+    chat(client, thread)
+
+
+if __name__ == "__main__":
+    main()
